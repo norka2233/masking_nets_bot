aab10ba9bd9d8426a59c809519b604f0e0478a39
Eleonora <norka2233@gmail.com>
2023-09-26 18:20:55 +0300

    token

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import telebot
-from config import TOKEN
+from secret_keys import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat_id, 'Вітаю, чим можу допомогти?')
+    bot.reply_to(message, 'Вітаю, чим можу допомогти?')
 
 
-
+bot.infinity_polling()
